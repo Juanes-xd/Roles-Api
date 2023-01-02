1775aabece5ffbd717b49d0a037f7041040d9569
Juanes-xd <juanestebanortizbejarano@gmail.com>
2023-01-02 14:07:34 -0500

    archivos js

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import morgan from "morgan";
+import productsRoutes from "./routes/products.routes.js";
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.json("welcome");
+});
+
+app.use(productsRoutes);
+
+export default app;
